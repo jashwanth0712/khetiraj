0df7824336c10da8fa43d05e4a94df269875040b
jashwanth0712 <jashwanth0712@gmail.com>
2022-12-27 15:23:36 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
+import '../../../utils/constants.dart';
+import 'package:ionicons/ionicons.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,17 +14,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'khetiraj',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,85 +25,173 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16,vertical: 10),
+            child: Text(
+              "Welcome to Plat OS...!",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+           const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16,vertical: 20),
+            child: Text(
+              "Sign Up",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w600,
+              ),
             ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16,vertical: 10),
+            child: Text(
+              "Let us help you to maintain your Farm",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+            inputField('Email', Icons.mail_outline_rounded),
+            inputField('Password', Icons.vpn_key_outlined),
+            inputField('Confirm Password', Icons.vpn_key_outlined),
+            loginButton('Sign Up'),
+            Text("Already have an Account?  Sign In"),
+            orDivider(),
+            logos(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Widget inputField(String hint, IconData iconData) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 8),
+    child: SizedBox(
+      height: 50,
+      child: Material(
+        elevation: 8,
+        shadowColor: Colors.black87,
+        color: Colors.transparent,
+        borderRadius: BorderRadius.circular(30),
+        child: TextField(
+          textAlignVertical: TextAlignVertical.bottom,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide.none,
+            ),
+            filled: true,
+            fillColor: Colors.white,
+            hintText: hint,
+            prefixIcon: Icon(iconData),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+Widget loginButton(String title) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 135, vertical: 16),
+    child: ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+        shape: const StadiumBorder(),
+        primary: kSecondaryColor,
+        elevation: 8,
+        shadowColor: Colors.black87,
+      ),
+      child: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+  );
+}
+
+Widget orDivider() {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 130, vertical: 8),
+    child: Row(
+      children: [
+        Flexible(
+          child: Container(
+            height: 1,
+            color: kPrimaryColor,
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            'or',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        Flexible(
+          child: Container(
+            height: 1,
+            color: kPrimaryColor,
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget logos() {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 16),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset('assets/images/facebook.png'),
+        const SizedBox(width: 24),
+        Image.asset('assets/images/google.png'),
+      ],
+    ),
+  );
+}
+
+Widget forgotPassword() {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 110),
+    child: TextButton(
+      onPressed: () {},
+      child: const Text(
+        'Forgot Password?',
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w600,
+          color: kSecondaryColor,
+        ),
+      ),
+    ),
+  );
 }
